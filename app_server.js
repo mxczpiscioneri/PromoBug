0baf2b7cbe288d2fb008927ca2a286ce9bb88753
Matheus Piscioneri <mxczpiscioneri@gmail.com>
2016-11-09 18:06:48 -0200

    Customize layout and create pagination

--- app_server.js
+++ app_server.js
@@ -7,19 +7,49 @@ var getProducts = require('./get_products.js');
 app.set('view engine', 'ejs');
 app.set('views', './views');
 
-app.get('/', function(req, res) {
+app.use(express.static('public'));
+app.use('/css', express.static(__dirname + '/node_modules/acsset/css'));
 
+app.get('/', function(req, res) {
 	Price.find({})
-		.sort({
-			percent: 'desc'
-		})
+		.sort([
+			['percent', 'desc'],
+			['name', 'asc']
+		])
 		.exec(function(err, prices) {
 			if (err) res.send(err);
 
 			res.render('index', {
-				data: prices
+				data: prices,
+				column: '',
+				order: ''
 			});
 		});
+});
+
+app.get('/list/:column/:order', function(req, res) {
+	var column = req.params.column;
+	var order = req.params.order;
+	var schema = Price.findOne().schema.obj;
+
+	if (schema.hasOwnProperty(column) && (order === 'asc' || order === 'desc')) {
+		Price.find({})
+			.sort([
+				[column, order],
+				['name', 'asc']
+			])
+			.exec(function(err, prices) {
+				if (err) res.send(err);
+
+				res.render('index', {
+					data: prices,
+					column: column,
+					order: order
+				});
+			});
+	} else {
+		res.redirect('/');
+	}
 
 });
 
